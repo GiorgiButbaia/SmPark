2d6bb571334dea964e2fa3ef97475efd68908fde
Gigi <gigibutbaia@yahoo.com>
2017-02-21 13:40:59 +0400

    Added index app

--- SmParkServer/SmPark/SmPark/settings.py
+++ SmParkServer/SmPark/SmPark/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'index', #Main (and possibly the only) app, handling video input
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
---
+++ SmParkServer/SmPark/index/urls.py
@@ -0,0 +1,3 @@
+from django.conf.urls import url, include
+#TODO fill in handlers/pages
+urlpatterns = []
